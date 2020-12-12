23681f6df21c39bb5b698ac838a6abb3501a8081
Ling Long <ling.long@mail.utoronto.ca>
2020-12-11 22:02:21 -0700

    Adding SPAC initial support and csv

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Discord bot which is supposed to check for SPACs?
 const Discord = require("discord.js");
-// const config = require("./config.json");
 const fetch = require("node-fetch");
+const csv = require('csv-parser');
+const fs = require('fs');
+require('dotenv').config();
 
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
 const prefix = "!";
 // Alpha Vantage API key
-const API_KEY =process.env.BOT_TOKEN.API_TOKEN;
-
+const API_KEY = process.env.API_TOKEN;
 const apiEndpoint = "https://www.alphavantage.co/query?apikey=" + API_KEY;
 
 // Current stock watchlist
 const watchlist = ['GIK', 'GIX', 'GHIV', 'AQB', 'APXT', 'THCB', 'AMD', 'BFT', 'TPGY'];
-
 client.on("message", async function(message) {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
@@ -73,6 +73,26 @@ client.on("message", async function(message) {
         }
         message.channel.send(embed);
     }
+
+    if (command === "s" || command === "spac" || command === "SPAC") {
+        if (args.length !== 1) {
+            message.reply("You must have exactly 1 argument after s, i.e. !s APXT");
+        } else {
+            const ticker = args[0];
+            const details = await getSPACDetails(ticker);
+            if (!details) {
+                message.channel.send("Failed to fetch ticker data! This command is for SPAC tickers only.")
+            } else {
+                const volume = await getStockVolume(ticker);
+                const embed = new Discord.MessageEmbed()     
+                    .setColor('#0099ff')
+                    .setTitle(ticker.toUpperCase()+ ': ' + details["Name"]);
+                embed.addField("Average Daily Volume", details["Average trading volume"].toString());
+                embed.addField("Today's Daily Volume", numberWithCommas(volume))
+                message.channel.send(embed);
+            }
+        }
+    }
 });
 
 /*
@@ -120,7 +140,6 @@ function convertMarketCapToString(marketCap) {
     }
 }
 
-
 /*
  * Returns the current stock price of a ticker. 
  */
@@ -136,4 +155,47 @@ function getStockPrice(ticker) {
         const globalQuote = newjson["Global Quote"];
         return globalQuote["05. price"];
     });
+}
+
+/*
+ * Returns the daily volume for a stock ticker.
+ */
+function getStockVolume(ticker) {
+    const quoteUrl = new URL(apiEndpoint);
+    quoteUrl.searchParams.set("function", "GLOBAL_QUOTE");
+    quoteUrl.searchParams.set("symbol", ticker);
+    return fetch(quoteUrl.toString()).then(async (newres) => {
+        const newjson = await newres.json();
+        if (!newjson["Global Quote"]) {
+            return "$$$";
+        }
+        const globalQuote = newjson["Global Quote"];
+        return globalQuote["06. volume"];
+    });
+}
+
+/*
+ * Returns an object containing details about an SPAC from the CSV file
+ */
+function getSPACDetails(ticker) {
+    let spac = 0;
+    // TODO: don't hardcode this later, fetch instead of reading from csv.
+    return new Promise((resolve, reject) => {
+        fs.createReadStream('spacs.csv')
+        .pipe(csv())
+        .on('data', (row) => {
+            if (row.Symbol.toLowerCase() === ticker.toLowerCase()) {
+                spac = row;
+            }
+        }).on('end', function () {
+            resolve(spac);
+        })
+    });
+}
+
+/*
+ * Formats a string with comma separators for the thousands places. 
+ */
+function numberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
